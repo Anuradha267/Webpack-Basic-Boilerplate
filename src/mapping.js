0e09673aac6453624ae6eea70ab37190701352d1
Anuradha267 <83057148+Anuradha267@users.noreply.github.com>
2022-01-19 16:31:17 +0530

    Create mapping.js

---
+++ src/mapping.js
@@ -0,0 +1,6 @@
+const Mapping = {
+   "BasicInputs": {'id':"BasicInput", 'thumbnail':'images/blue-blob.svg'},
+   "BoilerplateComponents":{'id':"BoilerplateComponent", 'thumbnail':'images/yellow-blob.png'}
+}
+
+export default Mapping;
